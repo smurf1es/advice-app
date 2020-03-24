a9fa5a635f356e8114e1a7f6d0629d5d9637db7d
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-03-24 11:27:31 +0700

    installed axios, added styling, get request to api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// CSS Link
+import "./App.css";
+
+class App extends Component {
+  state = {
+    advice: ""
+  };
+
+  componentDidMount() {
+    console.log("COMPONENT MOUNTED");
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then(res => {
+        const { advice } = res.data.slip;
+        this.setState({ advice });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+
+    return (
+      <Fragment>
+        <div className="app">
+          <div className="title">
+            <h1>Advice Generator.</h1>
+          </div>
+          <div className="card">
+            <h1 className="heading">{advice}</h1>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
